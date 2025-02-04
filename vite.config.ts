6eb438909bf83e6e0418afc919a2493f969382c5
Joop Ringelberg <joop.ringelberg@gmail.com>
2025-02-04 19:58:46 +0100

    vite config now has commonjs plugin and provides aliases for perspectives-core.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths';
+import commonjs from 'vite-plugin-commonjs';
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: 5177, // Stel een vaste poort in
   },
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths(), commonjs()],
+  resolve: {
+    alias: {
+      'perspectives-core': '/Users/joopringelberg/Code/perspectives-core/dist/perspectives-core.js',
+      '/perspectives-core.js': '/Users/joopringelberg/Code/perspectives-core/dist/perspectives-core.js'
+    }
+  }
 })
